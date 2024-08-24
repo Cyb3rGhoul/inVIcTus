302ff5c7d6bf4b142f352784450b431a7f44a063
Harsh Shukla <harsh19112003@gmail.com>
2024-08-24 09:19:18 +0530

    FIX: remove bg

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Landing.css';
-import backgroundVideo from '../assets/background.mp4';
 
 const Landing = () => {
     return (
